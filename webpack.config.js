468bdf78303f480ad1d75ea6ca8522b4bc7eae11
Guilherme Geisler <guilherme.sgeisler@gmail.com>
2026-02-27 17:44:54 -0300

    feat: adiciona suporte a CSS com css-loader e style-loader, remove arquivo de estilos index.css

--- webpack.config.js
+++ webpack.config.js
@@ -25,4 +25,13 @@ module.exports = {
     favicon: path.resolve("src", "assets", "scissors.svg"),
   }),
   ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 }
