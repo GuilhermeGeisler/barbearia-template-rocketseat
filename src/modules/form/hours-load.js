1e499746b63d5f827f49a7d7bb8f66d5a6a55790
Guilherme Geisler <guilherme.sgeisler@gmail.com>
2026-03-01 16:49:01 -0300

    feat: adiciona lógica para recarregar horários ao mudar a data e organiza imports

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -5,6 +5,10 @@ import { hoursClick } from "./hours-click.js";
 const hours = document.getElementById("hours");
 
 export function hoursLoad({ date }) {
+  // Limpa a lista de horários.
+  hours.innerHTML = "";
+
+  // Verifica quais horários estão disponíveis.
   const opening = openingHours.map((hour) => {
     // Recupera somente a hora.
     const [scheduleHour] = hour.split(":");
