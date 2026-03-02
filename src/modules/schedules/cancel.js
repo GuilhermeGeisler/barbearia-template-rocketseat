ed314e5c85aeeb6dc1cfa4b9ee86d84b5b81e226
Guilherme Geisler <guilherme.sgeisler@gmail.com>
2026-03-02 19:47:08 -0300

    feat: adiciona funcionalidade de cancelamento de agendamentos com confirmação e recarregamento

--- src/modules/schedules/cancel.js
+++ src/modules/schedules/cancel.js
@@ -1,18 +1,24 @@
+import { schedulesDay } from "./load.js";
+import { scheduleCancel } from "../../services/schedule-cancel.js";
 const period = document.querySelectorAll('.period');
 
 // Gera evento de click para cada período
 period.forEach((periods) => {
   // Captura o evento de click na lista.
-  periods.addEventListener('click', (event) => {
+  periods.addEventListener('click', async (event) => {
     if (event.target.classList.contains("cancel-icon")) {
       // Obtém a li pai do elemento clicado.
       const item = event.target.closest('li');
+      // Obtém o id do agendamento a partir do dataset da li.
       const { id } = item.dataset;
 
+      // Verifica se o id existe e solicita confirmação para cancelar o agendamento.
       if (id) {
         const isConfirm = confirm("Tem certeza que deseja cancelar esse agendamento?");
         if (isConfirm) {
-          
+          await scheduleCancel({ id });
+          // Recarrega  os agendamentos.
+          schedulesDay();
         }
       }
     }
