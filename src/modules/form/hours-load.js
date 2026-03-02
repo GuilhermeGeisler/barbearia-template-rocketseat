d471ae59fe1f1e8e4206f9cce9b68220ed7f4674
Guilherme Geisler <guilherme.sgeisler@gmail.com>
2026-03-02 19:20:28 -0300

    feat: atualiza a função hoursLoad para incluir agendamentos diários e corrige a verificação de horários disponíveis

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -4,9 +4,12 @@ import { hoursClick } from "./hours-click.js";
 
 const hours = document.getElementById("hours");
 
-export function hoursLoad({ date }) {
+export function hoursLoad({ date, dailySchedules }) {
   // Limpa a lista de horários.
   hours.innerHTML = "";
+
+  // Obtém a lista de horários ocupados.
+  const unavailableHours = dailySchedules.map((schedule) => dayjs(schedule.when).format("HH:mm"));
 
   // Verifica quais horários estão disponíveis.
   const opening = openingHours.map((hour) => {
@@ -14,11 +17,13 @@ export function hoursLoad({ date }) {
     const [scheduleHour] = hour.split(":");
 
     // Adiciona a hora na date e verifica se está no passado.
-    const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
+    const isHourPast = dayjs(date).add(scheduleHour, "hour").isBefore(dayjs())
+
+    const available = !unavailableHours.includes(hour) && !isHourPast;
 
     return {
       hour,
-      available: isHourPast,
+      available,
     }
   })
 
